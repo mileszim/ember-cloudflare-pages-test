8ed923a449924387c3b4bb9ee98f0addc7397e13
Miles Zimmerman <miles@zim.dev>
2024-11-18 16:20:40 -0800

    change location type

--- config/environment.js
+++ config/environment.js
@@ -5,7 +5,7 @@ module.exports = function (environment) {
     modulePrefix: 'ember-cloudflare-pages-test',
     environment,
     rootURL: '/',
-    locationType: 'history',
+    locationType: 'hash',
     EmberENV: {
       EXTEND_PROTOTYPES: false,
       FEATURES: {
